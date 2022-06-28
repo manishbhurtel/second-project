c21158c20cd7d3355582d5c5cd164b52183507ef
manishbhurtel <manishbhurtel668@gmail.com>
2022-06-28 17:45:48 +0545

    theme

--- src/components/Textbox.js
+++ src/components/Textbox.js
@@ -2,19 +2,89 @@ import React from "react";
 import { useState } from "react";
 import "./Textbox.css";
 import "./Toggle.css";
+import "./Themes.css"
+import img1 from "../images/blueMode.png"
+import img2 from "../images/redMode.png"
+import img3 from "../images/yellowMode.png"
 
 export default function Textbox(props) {
-  const[darkBody, setdarkBody] = useState({
+  
+  const [darkBody, setdarkBody] = useState({
     backgroundColor: 'rgb(243, 230, 255)',
     color: 'black'
   })
-  const[darkButton, setdarkButton] = useState({
+  const [darkButton, setdarkButton] = useState({
     backgroundColor: 'rgb(163, 64, 255)',
     color: 'white'
   })
-  const[darkText, setdarkText] = useState("Enable Dark Mode:")
-  const toggleButtonClicked = ()=>{
-    if(darkBody.color == 'black'){
+
+
+
+  //Themes need to be debugged as this code is not as efficient as it should have been.
+  //Code for theme section i.e. 3 color buttons, starts from here
+  const blueThemeClicked = () => {
+    if (darkBody.color == 'black' || darkBody.backgroundColor == 'yellow' || darkBody.backgroundColor == 'red'){
+      setdarkBody({
+        backgroundColor: 'rgb(93, 93, 255)',
+        color: 'white'
+      })
+      setdarkButton({
+        backgroundColor: 'white',
+        color: 'black'
+      })
+    }
+    else {
+      setdarkBody({
+        backgroundColor: 'rgb(243, 230, 255)',
+        color: 'black'
+      })
+      setdarkButton({
+        backgroundColor: 'rgb(163, 64, 255)',
+        color: 'white'
+      })
+    }
+  }
+  const redThemeClicked = () => {
+    if (darkBody.color == 'black') {
+      setdarkBody({
+        backgroundColor: 'rgb(255, 96, 96)',
+        color: 'white'
+      })
+    }
+    else {
+      setdarkBody({
+        backgroundColor: 'rgb(243, 230, 255)',
+        color: 'black'
+      })
+    }
+  }
+  const yellowThemeClicked = () => {
+    if (darkBody.color == 'black') {
+      setdarkBody({
+        backgroundColor: 'rgb(255, 255, 132)',
+        color: 'black'
+      })
+      
+    }
+    else {
+      setdarkBody({
+        backgroundColor: 'rgb(243, 230, 255)',
+        color: 'black'
+      })
+    }
+  }
+  //Code for theme section ends here
+
+
+
+
+  //Code for the toggle button to change page theme to black starts from here
+  const [darkText, setdarkText] = useState("Enable Dark Mode:")
+  const [darkThemeBorder, setdarkThemeBorder] = useState({
+    border: '2px solid black'
+  })
+  const toggleButtonClicked = () => {
+    if (darkBody.color == 'black') {
       setdarkBody({
         backgroundColor: 'black',
         color: 'white'
@@ -24,8 +94,11 @@ export default function Textbox(props) {
         color: 'black'
       })
       setdarkText("Enable Light Mode: ")
+      setdarkThemeBorder({
+        border: '2px solid white'
+      })
     }
-    else{
+    else {
       setdarkBody({
         backgroundColor: 'rgb(243, 230, 255)',
         color: 'black'
@@ -35,9 +108,18 @@ export default function Textbox(props) {
         color: 'white'
       })
       setdarkText("Enable Dark Mode: ")
+      setdarkThemeBorder({
+        border: '2px solid black'
+      })
     }
   }
-  
+  //Toggle code ends from here
+
+
+
+
+
+  //Code for the textarea, buttons starts from here
   const [text, settext] = useState("");
   const state_changed = (e) => {
     settext(e.target.value);
@@ -54,6 +136,9 @@ export default function Textbox(props) {
     let new_text = "";
     settext(new_text);
   };
+  //Code for the textarea, buttons ends from here
+
+
   return (
     <>
       <div className="main" style={darkBody}>
@@ -79,7 +164,16 @@ export default function Textbox(props) {
 
         <div className="toggle_responsive">
           <p className="toggle_para">{darkText}</p>
-          <input type="checkbox" className="checkbox" onClick={toggleButtonClicked}/>
+          <input type="checkbox" className="checkbox" onClick={toggleButtonClicked} />
+        </div>
+
+        <div className="customThemes">
+          <p className="themes_para">Enable Custom Themes: </p>
+          <div className="images_Themes" style={darkThemeBorder}>
+            <img src={img1} alt="Blue Color" class="img" onClick={blueThemeClicked} />
+            <img src={img2} alt="Red Color" class="img" onClick={redThemeClicked} />
+            <img src={img3} alt="Yellow Color" class="img" onClick={yellowThemeClicked} />
+          </div>
         </div>
 
         <div className="wac">
@@ -91,7 +185,7 @@ export default function Textbox(props) {
 
         <div className="minutes">
           <p className="minutes_para">
-            {0.18 * text.split(" ").length} second time is required to read the
+            {0.24 * text.split(" ").length} second time is required to read the
             above text.
           </p>
         </div>
